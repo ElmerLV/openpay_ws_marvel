575b64ef96b2f5b74e54d10e31edd0d50f35c9b0
Elmer LV <elmer.lopezv@gmail.com>
2024-02-09 16:16:44 -0700

    se suben pruebas unitarias

--- src/test/java/com/openpay/marvel/services/MarvelServicesTest.java
+++ src/test/java/com/openpay/marvel/services/MarvelServicesTest.java
@@ -31,5 +31,46 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MarvelServicesTest {
 
-    
+    @Mock
+    private MarvelRepository marvelRepository;
+
+    @Mock
+    private LoginRepository loginRepository;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private MarvelServiceImpl marvelService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testGetCharacters() {
+        LoginEntity loginEntity = new LoginEntity(/* Parámetros de prueba */);
+        Mockito.when(loginRepository.findByEmailAndPass(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(Optional.of(loginEntity));
+
+        Mockito.when(restTemplate.getForObject(Mockito.anyString(), Mockito.any()))
+                .thenReturn("{'name':'name','description':'description'}");
+
+        CharacterList result = marvelService.getCharacters(1L, "elmer.lopezv@gmail.com", "123abc");
+
+        assertNotNull(result);
+    }
+
+    @Test
+    void testGetConsultas() {
+        List<MarvelEntity> marvelEntities = new ArrayList<>();
+        Mockito.when(marvelRepository.findAll()).thenReturn(marvelEntities);
+
+        List<MarvelEntity> result = marvelService.getConsultas();
+
+        assertNotNull(result);
+        assertEquals(marvelEntities, result);
+    }
+
 }
